a2733e04f0fa0f4220bcd74c9e1278e38ea39099
Achyuth <achyuth.thumbalam@grootan.com>
2020-12-29 09:07:49 +0530

    Keycloak OIDC Flow

--- src/model/Config.js
+++ src/model/Config.js
@@ -1,6 +1,6 @@
 export const CookieName = "Keycloak_Auth";
 export const Config = {
     "url": "https://{hostname}/auth",
-    "realm": "your-keycloak-oidc-tenant-id",
-    "clientId": "your-keycloak-oidc-app-client-id"
+    "realm": "keycloak-tenant-id",
+    "clientId": "keycloak-client-id"
 }
